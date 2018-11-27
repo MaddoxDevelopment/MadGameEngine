493865657c47bdab6c1741152e39a6e379679a21
MaddoxDevelopment <jm@madev.me>
2018-11-26 23:05:51 -0600

    dynamic object generation removed tuple

--- GameEngine/Program.cs
+++ GameEngine/Program.cs
@@ -19,18 +19,48 @@ namespace GameEngine
 		}
 	}
 
+	internal class GameObjectPosition
+	{
+		public float X { get; private set; }
+		public float Y { get; private set; }
+
+		public GameObjectPosition()
+		{
+			X = 0;
+			Y = 0;
+		}
+
+		public void Set(float x, float y)
+		{
+			X = x;
+			Y = y;
+		}
+		public void Left(float x) => X -= x;
+		public void Right(float x) => X += x;
+		public void Up(float y) => Y -= y;
+		public void Down(float y) => Y += y;
+	}
+
+	internal class GameObject
+	{
+		public Guid Id { get; } = Guid.NewGuid();
+		public GameObjectPosition Position { get; }
+
+		public GameObject()
+		{
+			Position = new GameObjectPosition();
+		}
+	
+	}
+
 	internal class Test : GameWindow
 	{
-		private Vector2[][] buffer;
+		private Vector2[][] objectVectors;
 
-		private readonly Dictionary<int, Tuple<float, float>> location = new Dictionary<int, Tuple<float, float>>
-		{
-			{ 0, new Tuple<float, float>(100f, 100f) },
-			{ 1, new Tuple<float, float>(500f, 500f) }
-		};
+		private readonly Dictionary<int, GameObject> objects = new Dictionary<int, GameObject>();
 
 		private Tuple<int, Rectangle> selectedTarget;
-		private readonly int[] _vbo = new int[2]; //vertex buffer objects
+		private readonly int[] _vbo = new int[10];
 
 		public Test(int width, int height, GraphicsMode mode) : base(width, height, mode)
 		{
@@ -38,47 +68,61 @@ namespace GameEngine
 
 		protected override void OnLoad(EventArgs e)
 		{
-			buffer = new[]
+			var offset = 300;
+			var vectors = new[]
 			{
-				new[]
-				{
-					new Vector2(0, 0),
-					new Vector2(200, 0),
-					new Vector2(200, 200),
-					new Vector2(0, 200)
-				},
-				new[]
-				{
-					new Vector2(0, 0),
-					new Vector2(200, 0),
-					new Vector2(200, 200),
-					new Vector2(0, 200)
-				}
+				new Vector2(0, 0),
+				new Vector2(200, 0),
+				new Vector2(200, 200),
+				new Vector2(0, 200)
 			};
-
+			var temp = new List<Vector2[]>();
+			for (var i = 0; i < 10; i++)
+			{
+				temp.Add(vectors);
+				objects.Add(i, new GameObject());
+				objects[i].Position.Down(100);
+				var multiplier = i * 100;
+				objects[i].Position.Right(multiplier + offset);
+			}
+			objectVectors = temp.ToArray();
 			SetBuffer();
 			base.OnLoad(e);
 		}
 
 		protected override void OnMouseDown(MouseButtonEventArgs e)
 		{
-			for (var i = 0; i < buffer.Length; i++)
+			for (var i = 0; i < objectVectors.Length; i++)
 			{
-				var translation = location[i];
-				var points = buffer[i].Select(w =>
-					new Point((int)((int)w.X + translation.Item1), (int)((int)w.Y + translation.Item2))).ToList();
+				var translation = objects[i];
+				var points = objectVectors[i].Select(w =>
+					new Point((int)((int)w.X + translation.Position.X), (int)((int)w.Y + translation.Position.Y))).ToList();
 				var minX = points.Min(p => p.X);
 				var minY = points.Min(p => p.Y);
 				var maxX = points.Max(p => p.X);
 				var maxY = points.Max(p => p.Y);
 				var rec = new Rectangle(new Point(minX, minY), new Size(maxX - minX, maxY - minY));
-				Console.WriteLine("Rec: " + JsonConvert.SerializeObject(points));
 				if (!rec.Contains(e.Position)) continue;
 				selectedTarget = new Tuple<int, Rectangle>(i, rec);
 				break;
 			}
-
 			base.OnMouseDown(e);
+		}
+
+		protected override void OnKeyDown(KeyboardKeyEventArgs e)
+		{
+			var state = e.Keyboard;
+			if (state.IsKeyDown(Key.Number1) && state.IsKeyDown(Key.Number2))
+			{
+				objects[0].Position.Left(10);
+				objects[0].Position.Up(10);
+			}
+			if (e.Key == Key.Number1)
+			{
+				objects[0].Position.Right(10);
+				objects[0].Position.Down(10);
+			}
+			base.OnKeyDown(e);
 		}
 
 		protected override void OnMouseMove(MouseMoveEventArgs e)
@@ -90,7 +134,7 @@ namespace GameEngine
 			var rect = selectedTarget.Item2;
 			var halfWidth = rect.Width / 2;
 			var halfHeight = rect.Height / 2;
-			location[selectedTarget.Item1] = new Tuple<float, float>(e.X - halfHeight, e.Y - halfWidth);
+			objects[selectedTarget.Item1].Position.Set(e.X - halfHeight, e.Y - halfWidth);
 			base.OnMouseMove(e);
 		}
 
@@ -102,17 +146,15 @@ namespace GameEngine
 
 		private void SetBuffer()
 		{
-			GL.GenBuffers(2, _vbo);
+			GL.GenBuffers(objects.Count, _vbo);
 			for (var i = 0; i < _vbo.Length; i++)
 			{
-				Console.WriteLine(i + " " + _vbo[i]);
-				var b = buffer[i];
+				var b = objectVectors[i];
 				GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo[i]);
 				GL.BufferData(BufferTarget.ArrayBuffer, new IntPtr(Vector2.SizeInBytes * b.Length), b,
 					BufferUsageHint.StaticDraw);
 			}
 		}
-
 
 		protected override void OnRenderFrame(FrameEventArgs e)
 		{
@@ -133,15 +175,15 @@ namespace GameEngine
 
 			for (var i = 0; i < _vbo.Length; i++)
 			{
-				var starting = location[i];
-				world = Matrix4.CreateTranslation(starting.Item1, starting.Item2, 0);
+				var starting = objects[i];
+				world = Matrix4.CreateTranslation(starting.Position.X, starting.Position.Y, 0);
 				var extracted = world.ExtractTranslation();
-				location[i] = new Tuple<float, float>(extracted.X, extracted.Y);
+				objects[i].Position.Set(extracted.X, extracted.Y);
 				GL.MatrixMode(MatrixMode.Modelview);
 				GL.LoadMatrix(ref world);
 				GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo[i]);
-				GL.VertexPointer(2, VertexPointerType.Float, Vector2.SizeInBytes * i, 0);
-				GL.DrawArrays(PrimitiveType.Quads, 0, buffer[i].Length);
+				GL.VertexPointer(2, VertexPointerType.Float, Vector2.SizeInBytes, 0);
+				GL.DrawArrays(PrimitiveType.Quads, 0, objectVectors[i].Length);
 			}
 
 			this.SwapBuffers();
